acae3c3673d4f0d7928fcd55ca36407d9b26bb47
Jaime <jaime.3801911856@ucaldas.edu.co>
2021-09-13 14:33:14 -0500

    Fruteria

---
+++ app/src/main/java/com/jg/empresafrutas/Empleados.java
@@ -0,0 +1,14 @@
+package com.jg.empresafrutas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Empleados extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_empleados);
+    }
+}
